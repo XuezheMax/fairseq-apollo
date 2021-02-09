9314a92be1bcd0f58de6cbc9fd1dc9fa694c19ae
Xuezhe Ma <max@Xuezhes-MacBook-Pro.local>
2021-02-09 14:27:38 -0800

    upgrade dict name

--- fairseq/modules/moon_layer.py
+++ fairseq/modules/moon_layer.py
@@ -103,6 +103,20 @@ class MoonEncoderLayer(nn.Module):
         return BottleNeck(input_dim, hid_dim, activation_fn, activation_dropout_p,
                           q_noise, qn_block_size, shift=False)
 
+    def upgrade_state_dict_named(self, state_dict, name):
+        """
+        Rename layer norm states from `...layer_norms.0.weight` to
+        `...self_attn_layer_norm.weight` and `...layer_norms.1.weight` to
+        `...final_layer_norm.weight`
+        """
+        layer_norm_map = {"0": "self_attn_layer_norm", "1": "final_layer_norm"}
+        for old, new in layer_norm_map.items():
+            for m in ("weight", "bias"):
+                k = "{}.layer_norms.{}.{}".format(name, old, m)
+                if k in state_dict:
+                    state_dict["{}.{}.{}".format(name, new, m)] = state_dict[k]
+                    del state_dict[k]
+
     def forward(self, x, encoder_padding_mask, attn_mask: Optional[Tensor] = None):
         """
         Args:
