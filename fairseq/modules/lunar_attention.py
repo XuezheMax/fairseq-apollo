aa50355f3bddb36659290240771775f52b99c59d
Xuezhe Ma <max@Xuezhes-MacBook-Pro.local>
2021-03-02 10:41:33 -0800

    softmax -> elu + 1

--- fairseq/modules/lunar_attention.py
+++ fairseq/modules/lunar_attention.py
@@ -130,9 +130,12 @@ class LunarMultiheadAttention(nn.Module):
         pq = pq.permute(1, 2, 0, 3) * self.pscaling
         # B x H x L x N
         pqc = pq.matmul(k)
+
+        clen = context.size(0)
         if context_padding_mask is not None:
+            clen = clen - context_padding_mask.sum(dim=1)
             pqc = pqc.masked_fill(context_padding_mask.unsqueeze(1).unsqueeze(2).to(torch.bool), float("-inf"))
-        pqc = F.softmax(pqc, dim=-1)
+        pqc = (F.elu(pqc) + 1.0) / clen
         pqc = self.attention_dropout_module(pqc)
         # B x H x L x K
         pc = torch.matmul(pqc, v)
