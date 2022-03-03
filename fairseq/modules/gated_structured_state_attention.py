925fe71b522ab49e99615b0cb560ed93b6c9b00e
Xuezhe <xuezhe@d22md-xma.local>
2022-03-02 22:39:41 -0800

    softmax -> relu2

--- fairseq/modules/gated_structured_state_attention.py
+++ fairseq/modules/gated_structured_state_attention.py
@@ -190,8 +190,8 @@ class GatedStructuredStateAttention(nn.Module):
         if before_softmax:
             return qk, v
 
-        # attn_weights = torch.square(F.relu(qk))
-        attn_weights = utils.softmax(qk, dim=-1, onnx_trace=self.onnx_trace)
+        attn_weights = torch.square(F.relu(qk))
+        # attn_weights = utils.softmax(qk, dim=-1, onnx_trace=self.onnx_trace)
         kernel = self.attention_dropout(attn_weights)
         v = self.hidden_dropout(v)
         # B x N x E -> B x N x D
