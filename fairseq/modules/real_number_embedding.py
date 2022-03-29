d92d9ebadb6b23aaaf300a870160fe815bf51c75
XuezheMax <xuezhe.ma@gmail.com>
2022-03-28 21:41:13 -0700

    real number embedding

--- fairseq/modules/real_number_embedding.py
+++ fairseq/modules/real_number_embedding.py
@@ -12,31 +12,18 @@ from fairseq.modules.scale_norm import ScaleNorm
 
 
 class RealNumberEmbedding(nn.Module):
-    def __init__(self, embedding_dim, norm_type=None, export=False):
+    def __init__(self, embedding_dim):
         super().__init__()
         self.embedding_dim = embedding_dim
         self.weight = Parameter(torch.Tensor(embedding_dim))
-        self.weight_norm = None
-        if norm_type is not None:
-            self.weight_norm = self.build_embedding_norm(embedding_dim, norm_type, export)
-
+        self.bias = Parameter(torch.Tensor(embedding_dim))
         self.reset_parameters()
 
     def reset_parameters(self):
         std = self.embedding_dim ** -0.5
         nn.init.normal_(self.weight, mean=0, std=std)
-
-    def build_embedding_norm(self, embedding_dim, norm_type, export):
-        if norm_type == 'layernorm':
-            return LayerNorm(embedding_dim, export=export)
-        elif norm_type == 'scalenorm':
-            return ScaleNorm(dim=0)
-        else:
-            raise ValueError('Unknown norm type: {}'.format(norm_type))
+        nn.init.constant_(self.bias, 0.0)
 
     def forward(self, x):
-        weight = self.weight
-        if self.weight_norm is not None:
-            weight = self.weight_norm(weight)
-
-        return x.unsqueeze(-1) * weight
+        return torch.addcmul(self.bias, x.unsqueeze(-1), self.weight)
+        # return x.unsqueeze(-1) * self.weight + self.bias
