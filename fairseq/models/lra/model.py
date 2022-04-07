2d91ef242069ca1c3770aae9454a8ec200e14367
Xuezhe <xuezhe@d22md-xma.local>
2022-04-07 12:35:55 -0700

    chunk size

--- fairseq/models/lra/model.py
+++ fairseq/models/lra/model.py
@@ -296,7 +296,7 @@ class LRAEncoder(FairseqEncoder):
                 attention_dropout=args.attention_dropout,
                 hidden_dropout=args.act_dropout,
                 norm_type=args.norm_type,
-                chunk_size=args.chunk_size,
+                chunk_size=getattr(args, 'chunk_size', -1),
                 truncation=getattr(args, 'truncation_length', None),
                 max_seq_len=args.max_positions,
                 sen_rep_type=getattr(args, 'sen_rep_type', 'cls')
