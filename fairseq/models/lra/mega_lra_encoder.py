9e68dc9d8ff44a07336f435fb84b4e48265757a5
Xuezhe <xuezhe@d22md-xma.local>
2022-04-07 16:25:43 -0700

    chunk size

--- fairseq/models/lra/mega_lra_encoder.py
+++ fairseq/models/lra/mega_lra_encoder.py
@@ -172,7 +172,7 @@ class MegaLRAEncoder(nn.Module):
 
         bsz, seq_len = tokens.size()
         if self.chunk_size > 0 and seq_len % self.chunk_size != 0:
-            assert self.embedding_type != 'sparse', 'for image the sequence length {} must be divided by chunk size {}'.format(seq_len, self.chunk_size)
+            assert self.embedding_type == 'sparse', 'for image the sequence length {} must be divided by chunk size {}'.format(seq_len, self.chunk_size)
 
             num_paddings = math.ceil(seq_len / self.chunk_size) * self.chunk_size - seq_len
             tokens = F.pad(tokens, (0, num_paddings), value=self.padding_idx)
