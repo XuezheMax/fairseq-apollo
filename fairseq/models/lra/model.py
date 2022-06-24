2be78d55c2aeef2242b15092c361ae2cfb22d8c7
XuezheMax <xuezhe.ma@gmail.com>
2022-06-24 11:21:59 -0700

    small pf32

--- fairseq/models/lra/model.py
+++ fairseq/models/lra/model.py
@@ -637,6 +637,27 @@ def mega_lra_pf32(args):
     base_architecture(args)
 
 
+@register_model_architecture('lra', 'mega_lra_pf32_small')
+def mega_lra_pf32_small(args):
+    args.apply_bert_init = getattr(args, 'apply_bert_init', False)
+    args.layer_type = getattr(args, 'layer_type', 'mega')
+    args.encoder_hidden_dim = getattr(args, 'encoder_hidden_dim', 128)
+    args.z_dim = getattr(args, 'z_dim', 32)
+    args.n_dim = getattr(args, 'n_dim', 16)
+    args.encoder_layers = getattr(args, 'encoder_layers', 6)
+    args.activation_fn = getattr(args, 'activation_fn', 'silu')
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 64)
+    args.norm_type = getattr(args, 'norm_type', 'batchnorm')
+    args.classifier_layers = getattr(args, 'classifier_layers', 1)
+    args.classifier_out_dim = getattr(args, 'classifier_out_dim', 128)
+    args.sentence_class_num = getattr(args, 'sentence_class_num', 2)
+    args.chunk_size = getattr(args, 'chunk_size', 128)
+    args.truncation_length = getattr(args, 'truncation_length', 1024)
+    args.max_positions = getattr(args, 'max_positions', 1024)
+    args.sen_rep_type = getattr(args, 'sen_rep_type', 'mp')
+    base_architecture(args)
+
+
 @register_model_architecture('lra', 'luna_lra_pf128')
 def luna_lra_pf32(args):
     args.max_positions = getattr(args, 'max_positions', 128 * 128 + 2)
