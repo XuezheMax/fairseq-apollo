4e8d2301156f7e110fed801c04795e65971f848c
XuezheMax <xuezhe.ma@gmail.com>
2022-11-21 13:57:07 -0800

    dev

---
+++ fairseq/modules/base_moving_average.py
@@ -0,0 +1,95 @@
+# Author: Xuezhe Ma (Max)
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import math
+from typing import Dict, Optional, Tuple
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
+from torch.nn import Parameter
+
+from fairseq import utils
+from fairseq.incremental_decoding_utils import with_incremental_state
+
+
+@with_incremental_state
+class BaseMovingLayer(nn.Module):
+    """Legendre Moving Average Layer.
+
+    See "" for more details.
+    """
+
+    def __init__(self,):
+        super().__init__()
+
+    def prepare_for_onnx_export_(self):
+        self.onnx_trace = True
+
+    def prepare_for_tpu_(self, **kwargs):
+        self.tpu = True
+
+    def _calc_coeffs(self):
+        raise NotImplementedError
+
+    def _compute_kernel(self, length: int):
+        raise NotImplementedError
+
+    def coeffs(self):
+        if self.training:
+            return self._calc_coeffs()
+        else:
+            if self._coeffs is None:
+                self._coeffs = self._calc_coeffs()
+            return self._coeffs
+
+    def kernel(self, length: int):
+        kernel_size = length if self.truncation is None or self.truncation < 1 else min(self.truncation, length)
+        if self.training:
+            return self._compute_kernel(kernel_size)
+        else:
+            if self._kernel is None or self._kernel.size(-1) < kernel_size:
+                self._kernel = self._compute_kernel(kernel_size)
+            return self._kernel[..., :kernel_size]
+
+    def forward(
+        self,
+        x,
+        padding_mask: Optional[Tensor] = None,
+        incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]] = None,
+    ) -> Tensor:
+        """Input shape: Time x Batch x Channel
+
+        Args:
+            padding_mask (ByteTensor, optional): mask to exclude
+                keys that are pads, of shape `(batch, src_len)`, where
+                padding elements are indicated by 1s.
+        """
+        raise NotImplementedError
+
+    def _get_input_buffer(self, incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]]) -> Dict[str, Optional[Tensor]]:
+        result = self.get_incremental_state(incremental_state, "ema_state")
+        if result is not None:
+            return result
+        else:
+            empty_result: Dict[str, Optional[Tensor]] = {}
+            return empty_result
+
+    def _set_input_buffer(self, incremental_state: Dict[str, Dict[str, Optional[Tensor]]], buffer: Dict[str, Optional[Tensor]]):
+        return self.set_incremental_state(incremental_state, "ema_state", buffer)
+
+    @torch.jit.export
+    def reorder_incremental_state(
+            self, incremental_state: Dict[str, Dict[str, Optional[Tensor]]], new_order: Tensor
+    ):
+        """Reorder buffered internal state (for incremental generation)."""
+        input_buffer = self._get_input_buffer(incremental_state)
+        if input_buffer is not None:
+            for k in input_buffer.keys():
+                input_buffer_k = input_buffer[k]
+                if input_buffer_k is not None:
+                    input_buffer[k] = input_buffer_k.index_select(0, new_order)
+            incremental_state = self._set_input_buffer(incremental_state, input_buffer)
+        return incremental_state
