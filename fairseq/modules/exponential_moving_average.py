c8574b01c334448c6cfabbab626291891d5fa506
XuezheMax <xuezhe.ma@gmail.com>
2022-11-21 15:26:10 -0800

    beta in p

--- fairseq/modules/exponential_moving_average.py
+++ fairseq/modules/exponential_moving_average.py
@@ -77,17 +77,19 @@ class MultiHeadEMA(nn.Module):
         delta = torch.sigmoid(self.delta)
         q = 1.0 - p * delta
         p = p * self.beta
-        return p, q
+        # D x N
+        gamma = self.gamma * self.scale
+        return p, q, gamma
 
     def _compute_kernel(self, length: int):
         self._kernel = None
         # D x N x 1
-        p, q = self._calc_coeffs()
+        p, q, gamma = self._calc_coeffs()
         # D x N x L
         vander = torch.arange(length).to(q).view(1, 1, length) * torch.log(q)
         kernel = p * torch.exp(vander)
         # D x L
-        return torch.einsum('dnl,dn->dl', kernel, self.gamma * self.scale)
+        return torch.einsum('dnl,dn->dl', kernel, gamma)
 
     def coeffs(self):
         if self.training:
@@ -111,13 +113,13 @@ class MultiHeadEMA(nn.Module):
             return self.one_step(x, hx=hx)
 
         # D x N x 1
-        p, q = self.coeffs()
+        p, q, gamma = self.coeffs()
         # D x N x L+1
         vander = torch.arange(length + 1).to(p).view(1, 1, length + 1) * torch.log(q)
         vander = torch.exp(vander)
         if hx is not None:
             # D x N x L * D x N x 1 -> D x N x L
-            k = vander[:, :, 1:] * (self.gamma * self.scale).unsqueeze(-1)
+            k = vander[:, :, 1:] * gamma.unsqueeze(-1)
             ox = torch.einsum('bdn,dnl->bdl', hx, k)
             # D x N * B x D x N -> B x D x N
             hh = vander[:, :, -1] * hx
@@ -128,7 +130,7 @@ class MultiHeadEMA(nn.Module):
         # D x N x L
         vander = vander[:, :, :-1]
         kernel = p * vander
-        k = torch.einsum('dnl,dn->dl', kernel, self.gamma * self.scale)
+        k = torch.einsum('dnl,dn->dl', kernel, gamma)
 
         k_f = torch.fft.rfft(k.float(), n=2 * length)
         x_f = torch.fft.rfft(x.float(), n=2 * length)
@@ -138,20 +140,20 @@ class MultiHeadEMA(nn.Module):
         if ox is not None:
             out = out + ox
 
-        h = torch.einsum('bdl,dnl->bdn', x, torch.flip(kernel, dims=[2]))
+        h = torch.einsum('bdl,dnl->bdn', x.type_as(kernel), torch.flip(kernel, dims=[2]))
         if hh is not None:
             h = h + hh
         # L x B x D, B x D x N
         return out.permute(2, 0, 1), h
 
     def one_step(self, x, hx=None):
-        p, q = self.coeffs()
+        p, q, gamma = self.coeffs()
         # (D x N) x (B x D x 1) -> B x D x N
         h = p.squeeze(-1) * x
         if hx is not None:
             h = h + q.squeeze(-1) * hx
         # B x D
-        out = torch.einsum('bdn,dn->bd', h, self.gamma * self.scale)
+        out = torch.einsum('bdn,dn->bd', h, gamma)
         # 1 x B x D, B x D x N
         return out.unsqueeze(0), h
 
