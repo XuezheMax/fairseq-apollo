de9b9335f51bc30861f8fdcf1dd8c55037cfefd7
violet-zct <chunting.violet.zhou@gmail.com>
2023-01-29 22:13:01 -0800

    float32

--- fairseq/data/indexed_dataset.py
+++ fairseq/data/indexed_dataset.py
@@ -86,7 +86,7 @@ dtypes = {
     3: np.int16,
     4: np.int32,
     5: np.int64,
-    6: np.float,
+    6: np.float32,
     7: np.double,
     8: np.uint16
 }
@@ -289,7 +289,7 @@ class IndexedDatasetBuilder(object):
         np.int16: 2,
         np.int32: 4,
         np.int64: 8,
-        np.float: 4,
+        np.float32: 4,
         np.double: 8
     }
 
