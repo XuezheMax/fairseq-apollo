cadbdbbca4f37c226fee4a9db9cb507594da3ca0
violet-zct <chunting.violet.zhou@gmail.com>
2023-02-10 17:15:10 -0800

    gated cross attention

--- fairseq/modules/gated_cross_attention.py
+++ fairseq/modules/gated_cross_attention.py
@@ -59,9 +59,9 @@ class GatedCrossAttention(nn.Module):
         else:
             raise ValueError('unknown norm type: {}'.format(norm_type))
 
+        self.k_proj = nn.Linear(embed_dim, zdim, bias=True)
         self.v_proj = nn.Linear(embed_dim, embed_dim, bias=True)
-        self.z_proj = nn.Linear(embed_dim, zdim, bias=True)
-        self.ru_proj = nn.Linear(embed_dim, 2 * embed_dim, bias=True)
+        self.q_proj = nn.Linear(embed_dim, 2 * embed_dim + zdim, bias=True)
         self.h_proj = nn.Linear(embed_dim, embed_dim, bias=True)
         self.gamma = Parameter(torch.Tensor(2, zdim))
         self.beta = Parameter(torch.Tensor(2, zdim))
@@ -90,25 +90,25 @@ class GatedCrossAttention(nn.Module):
         # weights
         if mode == 'gaussian':
             std = 0.02
+            nn.init.normal_(self.k_proj.weight, mean=0.0, std=std)
             nn.init.normal_(self.v_proj.weight, mean=0.0, std=std)
-            nn.init.normal_(self.z_proj.weight, mean=0.0, std=std)
-            nn.init.normal_(self.ru_proj.weight, mean=0.0, std=std)
+            nn.init.normal_(self.q_proj.weight, mean=0.0, std=std)
             nn.init.normal_(self.h_proj.weight, mean=0.0, std=std)
         elif mode == 'xavier':
+            nn.init.xavier_uniform_(self.k_proj.weight)
             nn.init.xavier_uniform_(self.v_proj.weight)
-            nn.init.xavier_uniform_(self.z_proj.weight)
-            nn.init.xavier_uniform_(self.ru_proj.weight)
+            nn.init.xavier_uniform_(self.q_proj.weight)
             nn.init.xavier_uniform_(self.h_proj.weight)
         else:
             raise ValueError('Unknown init mode: {}'.format(mode))
 
         # bias
+        nn.init.constant_(self.k_proj.bias, 0.0)
         nn.init.constant_(self.v_proj.bias, 0.0)
-        nn.init.constant_(self.z_proj.bias, 0.0)
-        nn.init.constant_(self.ru_proj.bias, 0.0)
+        nn.init.constant_(self.q_proj.bias, 0.0)
         nn.init.constant_(self.h_proj.bias, 0.0)
         # gamma & beta
-        nn.init.constant_(self.gamma, 1.0)
+        nn.init.constant_(self.gamma, 0.0)
         nn.init.constant_(self.beta, 0.0)
 
     def element_attention(self, q, k, key_padding_mask, pidx, before_attn_fn):
@@ -224,26 +224,26 @@ class GatedCrossAttention(nn.Module):
 
         q = self.norm(query)
 
-        # L2 x B x 2*D
-        ur = self.ru_proj(q)
-        u, r = torch.split(ur, [self.embed_dim, self.embed_dim], dim=-1)
+        # L2 x B x (2*D+S)
+        base = self.q_proj(q)
+        u, r, q = torch.split(base, [self.embed_dim, self.embed_dim, self.zdim], dim=-1)
+        # L2 x B x S
+        gamma = self.gamma + 1.0
+        q = F.normalize(q, p=2, dim=-1, eps=1e-5)
+        q = q * gamma[0] + self.beta[0]
+
         # L2 x B x D
         u = torch.sigmoid(u)
         r = F.silu(r)
-
-        # L2 x B x S
-        q = self.z_proj(q)
-        q = F.normalize(q, p=2, dim=-1, eps=1e-5)
-        q = q * self.gamma[0] + self.beta[0]
 
         if key is None:
             assert value is None
             k = v = None
         else:
             # L1 x B x S
-            k = self.z_proj(key)
+            k = self.k_proj(key)
             k = F.normalize(k, p=2, dim=-1, eps=1e-5)
-            k = k * self.gamma[1] + self.beta[1]
+            k = k * gamma[1] + self.beta[1]
             # L1 x B x D
             v = F.silu(self.v_proj(value))
 
