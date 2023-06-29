d018f164234fc201cdb5fbd0c900b9e0a8322cd0
xuezhemax <xuezhe.ma@gmail.com>
2023-06-28 23:15:19 -0700

    typo

--- fairseq/modules/norm_layer/timestep_norm.py
+++ fairseq/modules/norm_layer/timestep_norm.py
@@ -84,7 +84,7 @@ class TimestepNorm(nn.Module):
             num_groups = self.num_features
 
         self.register_buffer("prior_count", torch.tensor(prior_count, dtype=torch.int64))
-        self.register_parameter("prior_mean", Parameter(torch.zeros(num_groups, requires_grad=(prior_count > 0))))
+        self.register_parameter("prior_mean", Parameter(torch.zeros(num_groups), requires_grad=(prior_count > 0)))
         self.register_parameter("prior_logv", Parameter(torch.zeros(num_groups), requires_grad=(prior_count > 0)))
         self.register_parameter("weight", Parameter(torch.zeros(num_features), requires_grad=True))
         self.register_parameter("bias", Parameter(torch.zeros(num_features), requires_grad=True))
