b933e77b3d3f11730f13c18c60ca455bf4151add
xuezhemax <xuezhe.ma@gmail.com>
2023-06-30 15:09:41 -0700

    simplied chain comparison

--- fairseq/models/lra/mega_lra_encoder.py
+++ fairseq/models/lra/mega_lra_encoder.py
@@ -145,7 +145,7 @@ class MegaLRAEncoder(nn.Module):
     ) -> Tuple[Union[torch.Tensor, List[torch.Tensor]], torch.Tensor]:
 
         bsz, seq_len = tokens.size()
-        if self.chunk_size > 0 and seq_len > self.chunk_size and seq_len % self.chunk_size != 0:
+        if seq_len > self.chunk_size > 0 != seq_len % self.chunk_size:
             assert self.embedding_type == 'sparse', 'for image the sequence length {} must be divided by chunk size {}'.format(seq_len, self.chunk_size)
 
             num_paddings = math.ceil(seq_len / self.chunk_size) * self.chunk_size - seq_len
