dd136fe7eb1195af75a8995230fc8a21862d12b1
xuezhemax <xuezhe.ma@gmail.com>
2023-07-13 21:54:00 -0400

    bidirectional fftconv

--- fairseq/modules/fused_ops/fftconv.py
+++ fairseq/modules/fused_ops/fftconv.py
@@ -1,7 +1,7 @@
 from typing import Tuple, Optional
 
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 
 from torch.autograd.function import FunctionCtx
 
@@ -11,10 +11,10 @@ from fairseq.mega2_extension.ops import rfft, fftconv_fwd, fftconv_bwd
 class FFTConvFunc(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx: FunctionCtx, x: torch.Tensor, k: torch.Tensor, bidirectional: bool = False) -> torch.Tensor:
+    def forward(ctx: FunctionCtx, x: torch.Tensor, k: torch.Tensor) -> torch.Tensor:
         L = x.size(-1)
-        if bidirectional or not x.is_cuda or L < 32 or L > 8192:
-            y, x_f, k_f = _fftconv_fwd(x, k, bidirectional)
+        if not x.is_cuda or L < 32 or L > 8192:
+            y, x_f, k_f = _fftconv_fwd(x, k)
         else:
             N = L if (L & (L - 1)) == 0 else (1 << L.bit_length())
             if k.dtype == torch.float32 or k.dtype == torch.float64:
@@ -24,7 +24,6 @@ class FFTConvFunc(torch.autograd.Function):
             y, x_f = fftconv_fwd(x, k_f)
         ctx.save_for_backward(x_f, k_f)
         ctx.k_dtype = k.dtype  # k_dtype is not a torch.Tensor
-        ctx.bidirectional = bidirectional
         return y
 
     @staticmethod
@@ -32,40 +31,29 @@ class FFTConvFunc(torch.autograd.Function):
         L = y_grad.size(-1)
         x_f, k_f = ctx.saved_tensors
         k_dtype = ctx.k_dtype
-        bidirectional = ctx.bidirectional
 
-        if bidirectional or not y_grad.is_cuda or L < 32 or L > 8192:
-            x_grad, k_grad = _fftconv_bwd(y_grad, x_f, k_f, bidirectional)
+        if not y_grad.is_cuda or L < 32 or L > 8192:
+            return _fftconv_bwd(y_grad, x_f, k_f)
         else:
-            x_grad, k_grad = fftconv_bwd(y_grad, x_f, k_f, k_dtype)
-
-        return x_grad, k_grad, None
+            return fftconv_bwd(y_grad, x_f, k_f, k_dtype)
 
 
 fftconv = FFTConvFunc.apply
 
 
 # @torch.jit.script
-def _fftconv_fwd(x: torch.Tensor, k: torch.Tensor,
-                 bidirectional: bool) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+def _fftconv_fwd(x: torch.Tensor, k: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     L: int = x.size(-1)
-    K: int = k.size(-1)
-    if bidirectional:
-        assert L * 2 == K
-    else:
-        assert L == K
-
-    N: int = 2 * L
-    N = N if (N & (N - 1)) == 0 else (1 << N.bit_length())
+    N: int = L if (L & (L - 1)) == 0 else (1 << L.bit_length())
     dtype = x.dtype
 
     if dtype == torch.float16 or dtype == torch.bfloat16:
         x = x.float()
 
-    x_f = torch.fft.rfft(x, n=N)
-    k_f = torch.fft.rfft(k, n=N, norm="forward")
+    x_f = torch.fft.rfft(x, n=2 * N)
+    k_f = torch.fft.rfft(k, n=2 * N, norm="forward")
     y_f = x_f * k_f
-    y = torch.fft.irfft(y_f, n=N, norm="forward")[..., :L]
+    y = torch.fft.irfft(y_f, n=2 * N, norm="forward")[..., :L]
 
     if dtype == torch.float16 or dtype == torch.bfloat16:
         y = y.to(dtype)
@@ -74,24 +62,39 @@ def _fftconv_fwd(x: torch.Tensor, k: torch.Tensor,
 
 
 # @torch.jit.script
-def _fftconv_bwd(y_grad: torch.Tensor, x_f: torch.Tensor, k_f: torch.Tensor,
-                 bidirectional: bool) -> Tuple[torch.Tensor, torch.Tensor]:
+def _fftconv_bwd(y_grad: torch.Tensor, x_f: torch.Tensor, k_f: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
     L: int = y_grad.size(-1)
-    K: int = 2 * L if bidirectional else L
-    N: int = 2 * L
-    N = N if (N & (N - 1)) == 0 else (1 << N.bit_length())
+    N: int = L if (L & (L - 1)) == 0 else (1 << L.bit_length())
 
     dtype = y_grad.dtype
     if dtype == torch.float16 or dtype == torch.bfloat16:
         y_grad = y_grad.float()
 
-    y_grad_f = torch.fft.rfft(y_grad.flip(dims=(-1,)), n=N)
+    y_grad_f = torch.fft.rfft(y_grad.flip(dims=(-1,)), n=2 * N)
     x_grad_f = y_grad_f * k_f
     k_grad_f = y_grad_f * x_f
-    x_grad = torch.fft.irfft(x_grad_f, n=N, norm="forward")[..., :L].flip(dims=(-1,))
-    k_grad = torch.fft.irfft(k_grad_f.sum(dim=0), n=N)[..., :K].flip(dims=(-1,))
+    x_grad = torch.fft.irfft(x_grad_f, n=2 * N, norm="forward")[..., :L].flip(dims=(-1,))
+    k_grad = torch.fft.irfft(k_grad_f.sum(dim=0), n=2 * N)[..., :L].flip(dims=(-1,))
 
     if dtype == torch.float16 or dtype == torch.bfloat16:
         x_grad = x_grad.to(dtype)
 
     return x_grad, k_grad
+
+
+def bidirectional_fftconv(x, k, shift):
+    bsz, embed_dim, seq_len = x.size()
+    kernel_size = k.size(1)
+    fft_len = seq_len + kernel_size
+    k1, k2 = torch.split(k, [embed_dim, embed_dim], dim=0)
+    # D x K+L
+    if shift:
+        k = F.pad(k1, (0, seq_len)) + F.pad(k2.flip(-1), (seq_len, 0))
+    else:
+        k = F.pad(k1, (0, seq_len)) + F.pad(k2[:, :1], (0, fft_len - 1)) + F.pad(k2[:, 1:].flip(-1), (seq_len + 1, 0))
+
+    k_f = torch.fft.rfft(k, n=fft_len, norm="forward")
+    x_f = torch.fft.rfft(x.float(), n=fft_len)
+    # B x D x L
+    y = torch.fft.irfft(x_f * k_f, n=fft_len, norm="forward")[..., :seq_len].to(x)
+    return y
